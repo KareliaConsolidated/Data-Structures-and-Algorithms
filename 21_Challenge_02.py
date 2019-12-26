9569f9e31273ef593f34a6930cd889e0af143e07
Anubhav Karelia <kareliaanubhav@outlook.com>
2019-12-26 15:33:32 +0530

    Challenge 02

---
+++ 21_Challenge_02.py
@@ -0,0 +1,44 @@
+# Frequency Counter / Multiple Pointers - areThereDuplicates
+# Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.
+# You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+# Time - O(N) # Space - O(N)
+############### METHOD 01 - Frequency Counter ###################
+
+def areThereDuplicates(*args):
+	collections = {n:list(args).count(n) for n in list(args)}
+	for key in collections:
+		if collections[key] > 1:
+			return True
+	return False
+
+############### METHOD 02  - Multiple Pointers ###################
+
+def areThereDuplicates(*args):
+	args = sorted(list(args))
+	startpt = 0
+	nextpt = 1
+	while nextpt < len(args):
+		if args[startpt] == args[nextpt]:
+			return True
+		startpt += 1
+		nextpt += 1
+	return False
+
+############### METHOD 03 - Frequency Counter ###################
+
+def areThereDuplicates(*args):
+	args = list(args)
+	for num in args:
+		count  = args.count(num)
+		if count > 1:
+			return True
+	return False
+
+############### METHOD 04 - One Liner ###################
+
+def areThereDuplicates(*args):
+	return len(set(args)) != len(list(args))
+
+print(areThereDuplicates(1, 2, 3)) # False
+print(areThereDuplicates(2, 2, 1)) # True
+print(areThereDuplicates('a', 'b', 'c', 'a')) # True
