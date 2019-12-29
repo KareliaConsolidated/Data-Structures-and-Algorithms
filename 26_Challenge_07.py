11aa716f9c60dd599b1cf657455eb1892ce1431e
Anubhav Karelia <kareliaanubhav@outlook.com>
2019-12-29 07:32:58 +0530

    Challenge 07

---
+++ 26_Challenge_07.py
@@ -0,0 +1,23 @@
+# Sliding Window - findLongestSubstring
+# Write a function called findLongestSubstring, which accepts a string and returns the length of the longest substring with all distinct characters.
+
+def findLongestSubstring(stri):
+	longest = 0
+	seen = {}
+	start = 0
+
+	for i in range(len(stri)):
+		char = stri[i]
+		if char in seen:
+			start = max(start, seen[char])
+
+		longest = max(longest, i-start+1)
+		seen[char] = i+1
+	return longest
+
+print(findLongestSubstring('')) # 0
+print(findLongestSubstring('thisisawesome')) # 6
+print(findLongestSubstring('thecatinthehat')) # 7
+print(findLongestSubstring('bbbbb')) # 1
+print(findLongestSubstring('longestsubstring')) # 8
+print(findLongestSubstring('thisishowwedoit')) # 6
