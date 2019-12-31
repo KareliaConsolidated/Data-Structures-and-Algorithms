d51dd21ff84f819326df9ca30287c8527af3104b
Anubhav Karelia <kareliaanubhav@outlook.com>
2019-12-31 19:01:53 +0530

    Rotation Check

---
+++ 29_Challenge_10.py
@@ -0,0 +1,25 @@
+# Rotation Check 
+A = [1, 2, 3, 4, 5, 6, 7]
+B = [4, 5, 6, 7, 1, 2, 3]
+
+def is_rotation(A, B):
+	if len(A) != len(B):
+		return False
+	key = A[0]
+	key_index = -1
+	for i in range(len(B)):
+		if key == B[i]:
+			key_index = i
+			break
+
+	if key_index == -1:
+		return False
+
+	for i in range(len(A)):
+		j = (key_index+i) % len(A)
+		if A[i] != B[j]:
+			return False
+	return True
+
+print(is_rotation(A,B))
+
