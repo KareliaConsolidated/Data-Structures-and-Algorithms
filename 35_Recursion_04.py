d3e7d6ec15e2bf6ef8fbf328600e40f7aba1dc79
Anubhav Karelia <kareliaanubhav@outlook.com>
2020-01-02 16:03:20 +0530

    Recursion Exercise - Square

---
+++ 35_Recursion_04.py
@@ -0,0 +1,9 @@
+# Power
+# Write a function called power which accepts a base and an exponent. The function should return the power of the base to the exponent. This function should mimic the functionality of Math.pow()
+def power(num,exp):
+	if exp == 0: return 1
+	return num * power(num, exp-1)
+
+print(power(2,0)) # 1
+print(power(2,2)) # 4
+print(power(2,4)) # 16
