cd374b74f24250f12492385ef1a27f21c345ae7a
Anubhav Karelia <kareliaanubhav@outlook.com>
2020-01-04 14:01:32 +0530

    Recursion: Reverse

---
+++ 40_Recursion_09.py
@@ -0,0 +1,6 @@
+# Write a recursive function called reverse which accepts a string and returns a new string in reverse
+def reverse(stri):
+	if len(stri) <= 1: return stri
+	return reverse(stri[1:]) + stri[0]
+
+print(reverse('awesome')) # emosewa
