52267e0db7a72aea2f11426d46ad68ba2611fac3
Anubhav Karelia <kareliaanubhav@outlook.com>
2020-03-12 15:17:35 +0530

    Multiple Pointers Example - SumZero

---
+++ 45_Practice/05_Practice_Code_Multiple_Pointers.py
@@ -0,0 +1,35 @@
+# Multiple Pointers
+# Creating pointers or values that correspond to an index or position and move towards the beginning, end or middle based on a certain condition
+# Very efficient for solving problems with minimal space complexity as well.
+
+# Write a function called sumZero which accepts a sorted array of integers. The function should find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist
+
+############### METHOD 01 ###############
+
+def sumZero(arr):
+	left = 0
+	right = len(arr)-1
+	while left < right:
+		arrsum = arr[left]+arr[right]
+		if arrsum == 0:
+			return [arr[left],arr[right]]
+		elif arrsum > 0:
+			right -= 1
+		else:
+			left += 1
+
+############### METHOD 02 ###############
+
+def sumZero(arr):
+	for val in arr:
+		if val == 0:
+			continue
+		if (-1 * val) in arr:
+			return [val, -val]
+
+#########################################
+
+print(sumZero([-4, -3, -2, -1, 0, 1, 2, 3, 10])) # [-3, 3]
+print(sumZero([-3, -2, -1, 0, 1, 2, 3])) # [-3, 3]
+print(sumZero([-2, 0, 1, 3])) # None
+print(sumZero([1, 2, 3])) # None			
