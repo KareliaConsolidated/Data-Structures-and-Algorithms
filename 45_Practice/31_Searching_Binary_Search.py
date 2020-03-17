dbd39bbf358cc5a91a9a5438b3bd26d380192a4c
Anubhav Karelia <kareliaanubhav@outlook.com>
2020-03-17 18:26:01 +0530

    Searching - Binary Search

--- 45_Practice/31_Searching_Binary_Search.py
+++ 45_Practice/31_Searching_Binary_Search.py
@@ -1,18 +1,20 @@
 def BinarySearch(arr,num):
 	start = 0
 	end = len(arr)-1
-	middle = ((start + end)//2)
-
+	middle = (start + end )//2
 	while start <= end:
-		if (num <= arr[middle]):
-			end = middle - 1
-		else:
+
+		if num > middle:
 			start = middle + 1
-		middle = ((start + end)//2)
-		if (arr[middle] == num):
-			return arr[middle]
+		else:
+			end = middle - 1
+		
+		middle = (start + end) // 2
+
+		if arr[middle] == num:
+			return middle
 	return -1
 
 numarray = [1,2,3,4,5,6,7,8,9,10]
 
-print(BinarySearch(numarray,11))
+print(BinarySearch(numarray,2))
