cb302ea96a8cf948647a06ef40f8fde38f4376ce
Anubhav Karelia <kareliaanubhav@outlook.com>
2020-04-02 19:46:35 +0530

    Naive String Search - (O(n^2))

---
+++ 45_Practice/33_Naive_String_Search.py
@@ -0,0 +1,9 @@
+def naive_string_search(longstr, shortstr):
+	count = 0
+	for i in range(len(longstr)):
+		for j in range(len(shortstr)):
+			if 
+
+
+print(naive_string_search('lorie loled', 'lol')) # 1
+print(naive_string_search('lorie loled', 'lo')) # 2 0
