c2a6af20c1fb105dfe3e0cab56ff8a94f45eabe4
Anubhav Karelia <kareliaanubhav@outlook.com>
2020-04-02 22:36:30 +0530

    Domain Extension

---
+++ 47_Random_Example_03.py
@@ -0,0 +1,10 @@
+# The email_list function receives a dictionary, which contains domain names as keys, and a list of users as values. Fill in the blanks to generate a list that contains complete email addresses (e.g. diana.prince@gmail.com).
+
+def email_list(domains):
+        emails = []
+        for names, users in domains.items():
+          for user in users:
+            emails.append(user+"@"+names)
+        return(emails)
+
+print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "yahoo.com": ["barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
