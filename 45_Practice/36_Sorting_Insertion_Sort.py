3bfbb11362683ca37bc991883f3ffef0a0612631
Anubhav Karelia <kareliaanubhav@outlook.com>
2020-04-03 18:17:21 +0530

    Insertion Sort Modified

--- 45_Practice/36_Sorting_Insertion_Sort.py
+++ 45_Practice/36_Sorting_Insertion_Sort.py
@@ -1,12 +1,11 @@
 def insertion_sort(arr):
 	for i in range(1,len(arr)):
-		currentVal = arr[i]
-
+		currentval = arr[i]
 		j = i-1
-		while j>=0 and currentVal < arr[j]:
+		while j>=0 and currentval<arr[j]:
 			arr[j+1] = arr[j]
 			j -= 1
-		arr[j+1] = currentVal
+		arr[j+1] = currentval
 	return arr
 
 print(insertion_sort([13,14,11,12,19,18,17,16,15])) # [11, 12, 13, 14, 15, 16, 17, 18, 19]
