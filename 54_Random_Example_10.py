5d8d6d06377a5fd018b353d4b5607c4f57d17e0c
Anubhav Karelia <kareliaanubhav@outlook.com>
2020-04-04 18:48:41 +0530

    Combine Guest Lists

---
+++ 54_Random_Example_10.py
@@ -0,0 +1,16 @@
+# Taylor and Rory are hosting a party. They sent out invitations, and each one collected responses into dictionaries, with names of their friends and how many guests each friend is bringing. Each dictionary is a partial list, but Rory's list has more current information about the number of guests. Fill in the blanks to combine both dictionaries into one, with each friend listed only once, and the number of guests from Rory's dictionary taking precedence, if a name is included in both dictionaries. Then print the resulting dictionary.
+
+from copy import deepcopy
+
+def combine_guests(guests1, guests2):
+    backup = deepcopy(guests1)
+    guests1.update(guests2)
+    for guest in guests1:
+        if guest in backup:
+            guests1[guest] = backup[guest]
+    return guests1
+
+Rorys_guests = { "Adam":2, "Brenda":3, "David":1, "Jose":3, "Charlotte":2, "Terry":1, "Robert":4}
+Taylors_guests = { "David":4, "Nancy":1, "Robert":2, "Adam":1, "Samantha":3, "Chris":5}
+
+print(combine_guests(Rorys_guests, Taylors_guests))
