7d5c17e30170ed717636126416041638ecd0008b
Anubhav Karelia <kareliaanubhav@outlook.com>
2020-04-08 19:20:08 +0530

    Merge Sort Completed

--- 45_Practice/37_Sorting_Merge_Sort.py
+++ 45_Practice/37_Sorting_Merge_Sort.py
@@ -8,7 +8,7 @@
 def merge(arr1, arr2):
 	
 	results = []
-	i, j = 0, 0
+	i = j = 0
 
 	while (i<len(arr1)) and (j<len(arr2)):
 		if arr2[j] > arr1[i]:
@@ -28,4 +28,18 @@ def merge(arr1, arr2):
 
 	return results
 
-print(merge([1,10,50],[2,14,99,100]))
+# print(merge([1,10,50],[2,14,99,100]))
+
+# Merge Sort Pseudocode
+# Break up the array into halves until you have arrays that are empty or have one element.
+# Once you have smaller sorted arrays, merge those arrays with other sorted arrays until you are back at the full length of the array
+# Once the array has been merged back together, return the merged (and sorted !) array.
+
+def mergeSort(arr):
+	if len(arr) <= 1: return arr
+	mid = len(arr) // 2
+	left = mergeSort(arr[:mid])
+	right = mergeSort(arr[mid:])
+	return merge(left, right)
+
+print(mergeSort([10,34,23,36,76,73,1,9,2]))	
