fc90f609b7b4349e889174f8362dde5a429b6fae
Anubhav Karelia <kareliaanubhav@outlook.com>
2020-05-04 21:05:27 +0530

    Frequency Counter - Anagrams

---
+++ 46_Practice/02_Frequency_Counter_Anagrams.py
@@ -0,0 +1,30 @@
+# Anagrams
+# Given two strings, write a function to determine if the second string is an anagram of the first. Ana anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema formed from iceman.
+########### METHOD 01 ##############
+
+def validAnagram(str1, str2):
+	if len(str1) != len(str2):
+		return False
+
+	charCount1 = {letter:str1.count(letter) for letter in str1}
+	charCount2 = {letter:str2.count(letter) for letter in str2}
+
+	for key, val in charCount1.items():
+		if key not in charCount2.keys():
+			return False
+		elif charCount1[key] != charCount2[key]:
+			return False
+	return True
+
+########### METHOD 02 ##############
+
+from collections import Counter
+def validAnagram(str1, str2):
+	if len(str1) != len(str2):
+		return False
+	return Counter(str1) == Counter(str2)
+
+print(validAnagram('','')) # True
+print(validAnagram('anagram','nagaram')) # True
+print(validAnagram('rat', 'car')) # False
+print(validAnagram('Qwerty', 'tyerwQ')) # True		
