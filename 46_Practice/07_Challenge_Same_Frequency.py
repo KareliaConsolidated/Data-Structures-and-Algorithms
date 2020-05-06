759ef9511bc8d448f381b7d2796f1284e1f660be
Anubhav Karelia <kareliaanubhav@outlook.com>
2020-05-06 21:56:41 +0530

    Challenge - Same Frequency

---
+++ 46_Practice/07_Challenge_Same_Frequency.py
@@ -0,0 +1,30 @@
+# Frequency Counter - sameFrequency
+# Write a function called sameFrequency. Given two positive integers, find out if the two numbers have the same frequency of digits. 
+# Time Complexity Should be O(N)
+
+############## METHOD 01 ##############
+
+from collections import Counter
+def sameFrequency(num1, num2): 
+	return Counter(str(num1)) == Counter(str(num2))
+
+############## METHOD 02 ##############	
+
+def sameFrequency(num1, num2):
+	if len(str(num1)) != len(str(num2)):
+		return False
+
+	numDict_01 = {i:str(num1).count(i) for i in str(num1)}
+	numDict_02 = {i:str(num2).count(i) for i in str(num2)}
+
+	for key, val in numDict_01.items():
+		if key not in numDict_02.keys():
+			return False
+		elif numDict_01[key] != numDict_02[key]:
+			return False
+	return True
+
+print(sameFrequency(182, 281)) # True
+print(sameFrequency(34, 14)) # False
+print(sameFrequency(3589578, 5879385)) # True
+print(sameFrequency(22, 222)) # False
