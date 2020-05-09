060992656aa19f7a6f3a32ca9ea5d1417c83d4cf
Anubhav Karelia <kareliaanubhav@outlook.com>
2020-05-09 22:15:00 +0530

    Challenge maxSubArraySum

---
+++ 46_Practice/11_Challenge_maxSubArraySum.py
@@ -0,0 +1,26 @@
+# Sliding Window - maxSubarraySum
+# Given an array of integers and a number, write a function called maxSubarraySum which finds the maximum sum of a subarray with the length of the number passed to the function.
+
+# Note that a subarray must consist of consecutive elements from the original array. In the first example below, [100,200,300] is a subarray of the original array, but [100,300] is not.
+
+def maxSubarraySum(arr, num):
+	if len(arr) < num: return None
+	maxSum = 0
+	tempSum = 0
+
+	for i in range(num):
+		maxSum += arr[i]
+
+	tempSum = maxSum
+
+	for j in range(num, len(arr)):
+		tempSum = tempSum - arr[j-num] + arr[j]
+		maxSum = max(tempSum,maxSum)
+
+	return maxSum
+
+print(maxSubarraySum([100,200,300,400], 2)) # 700
+print(maxSubarraySum([1,4,2,10,23,3,1,0,20], 4)) # 39
+print(maxSubarraySum([-3,40,-2,6,-1], 2)) # 38
+print(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1], 2)) # 5
+print(maxSubarraySum([2,3], 3)) # None
