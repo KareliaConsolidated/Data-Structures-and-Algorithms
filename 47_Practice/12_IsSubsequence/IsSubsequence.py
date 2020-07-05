cabee944de4937a61ef9f0380082cbb882a4e9e0
KareliaConsolidated <kareliaanubhav@outlook.com>
2020-07-05 19:02:29 +0530

    IsSubsequence

--- 47_Practice/12_IsSubsequence/IsSubsequence.py
+++ 47_Practice/12_IsSubsequence/IsSubsequence.py
@@ -2,6 +2,7 @@
 # Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string without their order changing.
 
 def isSubsequence(subStr, fullStr):
+	if len(subStr) > len(fullStr): return False
 	subPtr = 0
 	fullPtr = 0
 	while fullPtr < len(fullStr):
