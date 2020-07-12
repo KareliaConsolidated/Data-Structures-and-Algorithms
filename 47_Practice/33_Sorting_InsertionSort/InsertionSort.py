118e54cf15052a17d21c4a8d559b6c523a8ad9cf
KareliaConsolidated <kareliaanubhav@outlook.com>
2020-07-12 15:01:18 +0530

    Sorting -- 7Insertion Sort

---
+++ 47_Practice/33_Sorting_InsertionSort/InsertionSort.py
@@ -0,0 +1,9 @@
+def InsertionSort(arr):
+	for i in range(1, len(arr)):
+		currentVal = arr[i]
+		j = i - 1
+		while j>=0 and arr[j] > currentVal:
+			arr[j+1] = arr[j]
+			j -= 1 
+		arr[j+1] = currentVal
+	return arr
