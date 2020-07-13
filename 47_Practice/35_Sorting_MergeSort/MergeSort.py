36610f072e2932415aee5d1e00e329c12e1eea78
KareliaConsolidated <kareliaanubhav@outlook.com>
2020-07-13 20:20:49 +0530

    Sorting MergeSort

---
+++ 47_Practice/35_Sorting_MergeSort/MergeSort.py
@@ -0,0 +1,30 @@
+def MergeHelper(arr1, arr2):
+	i = 0
+	j = 0
+	NewArr = []
+	while i < len(arr1) and j < len(arr2):
+		if arr1[i] < arr2[j]:
+			NewArr.append(arr1[i])
+			i += 1
+		else:
+			NewArr.append(arr2[j])
+			j += 1
+
+	if j < len(arr2):
+		NewArr += arr2[j:]
+	elif i < len(arr1):
+		NewArr += arr1[i:]
+
+	return NewArr
+
+# print(MergeHelper([1,10,50],[2,14,99,100]))
+# print(MergeHelper([3],[1,2]))
+
+def MergeSort(arr):
+	if len(arr) <= 1: return arr
+	mid = len(arr) // 2
+	FirstArr, SecondArr = MergeSort(arr[:mid]), MergeSort(arr[mid:])
+	print(FirstArr, SecondArr)
+	return MergeHelper(FirstArr, SecondArr)
+
+print(MergeSort([5,4,3,1,2]))
