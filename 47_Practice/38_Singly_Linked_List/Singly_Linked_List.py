32dc5f81f06066a3e09aa6b67f30ff20364d1180
KareliaConsolidated <kareliaanubhav@outlook.com>
2020-07-20 15:36:46 +0530

    Singly Linked List

--- 47_Practice/38_Singly_Linked_List/Singly_Linked_List.py
+++ 47_Practice/38_Singly_Linked_List/Singly_Linked_List.py
@@ -3,45 +3,86 @@ class Node:
 		self.val = val
 		self.next = None
 
-class SinglyLinkedList:
+class SLL:
 	def __init__(self):
 		self.head = None
 		self.tail = None
 		self.length = 0
 
 	def push(self, val):
-		NewNode = Node(val)
-		if self.head is None:
-			self.head = NewNode
-			self.tail = self.head
+		newNode = Node(val)
+		if self.head == None:
+			self.head = newNode
+			self.tail = newNode
 		else:
-			self.tail.next = NewNode
-			self.tail = NewNode
+			self.tail.next = newNode
+			self.tail = newNode
 		self.length += 1
 		return self
 
 	def traverse(self):
-		current = self.head
-		while current is not None:
-			print(current.val)
-			current = current.next
+		if self.head is None: return None
+		currentNode = self.head
+		while currentNode is not None:
+			print(currentNode.val)
+			currentNode = currentNode.next
 
 	def pop(self):
 		if self.length == 0: return None
-		currNode = self.head
-		prevNode = currNode
-		while currNode.next is not None:
-			prevNode = currNode
-			currNode = currNode.next
-		self.tail = prevNode
+
+		currentNode = self.head
+		prevNode = currentNode
+
+		while currentNode.next is not None:
+			prevNode = currentNode
+			currentNode = currentNode.next
+
 		prevNode.next = None
+		self.tail = prevNode
+
+		if self.length == 1: self.head = None; self.tail = None
+
 		self.length -= 1
+
+		return currentNode
+
+	def shift(self):
+		if not self.head: return None 
+
+		currNode = self.head
+		self.head = currNode.next
+
+		self.length -= 1
+		if self.length == 0: self.tail = None
+
 		return currNode.val
 
+	def unshift(self, val):
+		newNode = Node(val)
+
+		if self.length == 0: self.head = newNode; self.tail= newNode
+
+		if self.length >= 1:
+			newNode.next = self.head
+			self.head = newNode
+
+		
+		self.length += 1
+		
+		return self
 
 
-SLL = SinglyLinkedList()
-SLL.push('A')
-#SLL.traverse()
-print(SLL.pop())
-#SLL.traverse()
+
+newNode = SLL()
+newNode.push(1).push(2).push(3).push(4)
+newNode.traverse()
+newNode.pop()
+newNode.pop()
+newNode.traverse()
+newNode.shift()
+newNode.traverse()
+newNode.shift()
+newNode.traverse()
+newNode.shift()
+newNode.unshift(56).unshift(67)
+newNode.traverse()
